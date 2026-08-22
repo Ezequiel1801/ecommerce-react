f98df7c6744c9ea5ed3ecf463fe2f75927c0a60e
Ezequiel Carrizo <ecarrizo075@gmail.com>
2026-08-22 00:36:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      id="container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hola mundo</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione debitis
+        distinctio quae consequuntur ut, molestias necessitatibus obcaecati
+        alias autem deleniti unde amet tempora dolorum? Tempora aspernatur
+        architecto vitae praesentium adipisci?
+      </p>
+    </div>
+  );
+};
+
+export default App;
